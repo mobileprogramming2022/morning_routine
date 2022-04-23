56a867e32e3a9d1a957898730742e3c96ef57da0
youngyun lee <60284649+hippohama@users.noreply.github.com>
2022-04-23 14:20:31 +0900

    morning routine layout and some intents

---
+++ app/src/main/java/com/gachon/morningroutin_layout/OptionActivity.java
@@ -0,0 +1,74 @@
+package com.gachon.morningroutin_layout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OptionActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option);
+
+        //image button
+        ImageButton main = findViewById(R.id.Plan);
+        main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivityForResult(intent, 101);
+                overridePendingTransition(0, 0);
+            }
+        });
+        ImageButton graph = findViewById(R.id.Graph);
+        graph.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), GraphActivity.class);
+                startActivityForResult(intent, 101);
+                overridePendingTransition(0, 0);
+            }
+        });
+        ImageButton reward = findViewById(R.id.Reward);
+        reward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RewardActivity.class);
+                startActivityForResult(intent, 101);
+                overridePendingTransition(0, 0);
+            }
+        });
+        //text for button
+        TextView txtMain = findViewById(R.id.textPlan);
+        txtMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivityForResult(intent, 101);
+                overridePendingTransition(0, 0);
+            }
+        });
+        TextView txtGraph = findViewById(R.id.textGraph);
+        txtGraph.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), GraphActivity.class);
+                startActivityForResult(intent, 101);
+                overridePendingTransition(0, 0);
+            }
+        });
+        TextView txtReward = findViewById(R.id.textReward);
+        txtReward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RewardActivity.class);
+                startActivityForResult(intent, 101);
+                overridePendingTransition(0, 0);
+            }
+        });
+    }
+}
