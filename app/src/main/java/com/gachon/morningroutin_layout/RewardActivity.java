9c9c36a51bdb52cfced89e4bcbad58f845351720
youngyun lee <60284649+hippohama@users.noreply.github.com>
2022-04-29 20:38:33 +0900

    make drag and drop image
    some try for using firebase but not applying yet

--- app/src/main/java/com/gachon/morningroutin_layout/RewardActivity.java
+++ app/src/main/java/com/gachon/morningroutin_layout/RewardActivity.java
@@ -1,12 +1,21 @@
 package com.gachon.morningroutin_layout;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.DragEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,15 +30,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class RewardActivity  extends AppCompatActivity {
+public class RewardActivity  extends AppCompatActivity implements View.OnLongClickListener, View.OnDragListener {
     private static final String TAG = "rewardTag";
     // get instances for firebase database
     FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     DatabaseReference databaseRef = firebaseDatabase.getReference();
     int tree01, tree02, tree03, tree04, tree05, tree06, tree07, tree08, tree09;
     TextView inv01, inv02, inv03, inv04, inv05, inv06, inv07, inv08, inv09;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,9 +82,22 @@ public class RewardActivity  extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Error connecting DB", Toast.LENGTH_SHORT).show();
             }
         });
+        ImageView item[]=new ImageView[9];
+        //set image for drag
+        for (int i=1;i<=9; i++) {
+            int k = getResources().getIdentifier("item0" + i,"id", getPackageName());
+            item[i-1] = (ImageView) findViewById(k);
+            item[i-1].setTag("ANDROID ICON");
+            item[i-1].setOnLongClickListener(this);
+        }
 
-
-
+        //set table for switch
+        for (int i=1;i<=4;i++) {
+            for (int j=1;j<=5;j++) {
+                int k = getResources().getIdentifier("imageView" + i + "_" + j, "id", getPackageName());
+                findViewById(k).setOnDragListener(this);
+            }
+        }
 
         // bottom menu
         //image button
@@ -136,5 +156,67 @@ public class RewardActivity  extends AppCompatActivity {
                 overridePendingTransition(0, 0);
             }
         });
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        // Create a new ClipData.Item from the ImageView object's tag
+        ClipData.Item item = new ClipData.Item((CharSequence) view.getTag());
+        // Create a new ClipData using the tag as a label, the plain text MIME type, and
+        // the already-created item. This will create a new ClipDescription object within the
+        // ClipData, and set its MIME type entry to "text/plain"
+        String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+        ClipData data = new ClipData(view.getTag().toString(), mimeTypes, item);
+        // Instantiates the drag shadow builder.
+        View.DragShadowBuilder dragShadow = new View.DragShadowBuilder(view);
+        // Starts the drag
+        view.startDrag(data        // data to be dragged
+                , dragShadow   // drag shadow builder
+                , view           // local data about the drag and drop operation
+                , 0          // flags (not currently used, set to 0)
+        );
+        return true;
+    }
+
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+        int action = event.getAction();
+        switch (action) {
+            case DragEvent.ACTION_DRAG_STARTED:
+                if (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+                    return true;
+                }
+                return false;
+                //some events if we can use
+            case DragEvent.ACTION_DRAG_ENTERED:
+                return true;
+            case DragEvent.ACTION_DRAG_LOCATION:
+                return true;
+            case DragEvent.ACTION_DRAG_EXITED:
+                return true;
+
+            case DragEvent.ACTION_DROP:
+                ClipData.Item item = event.getClipData().getItemAt(0);
+                String dragData = item.getText().toString();
+                View vw = (View) event.getLocalState();
+                ImageView setting= (ImageView)v;
+                ImageView resource=(ImageView) vw;
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) resource.getDrawable();
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                setting.setImageBitmap(bitmap);
+                //should be code change for count --
+                ViewGroup owner = (ViewGroup) vw.getParent();
+                owner.removeView(vw); //remove the dragged view
+                // Returns true. DragEvent.getResult() will return true.
+                return true;
+
+            case DragEvent.ACTION_DRAG_ENDED:
+                return true;
+            // An unknown action type was received.
+            default:
+                Log.e("DragDrop Example", "Unknown action type received by OnDragListener.");
+                break;
+        }
+        return false;
     }
 }
