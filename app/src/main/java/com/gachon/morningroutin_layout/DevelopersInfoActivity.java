28bc41ecebfa6c9dc67c80ff6bc9cd024d1286ca
Hyunmin.Cho <71583831+Hyunmin-jasper-Cho@users.noreply.github.com>
2022-05-19 20:14:36 +0900

    addOptionGui

--- app/src/main/java/com/gachon/morningroutin_layout/DevelopersInfoActivity.java
+++ app/src/main/java/com/gachon/morningroutin_layout/DevelopersInfoActivity.java
@@ -3,6 +3,8 @@ package com.gachon.morningroutin_layout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DevelopersInfoActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class DevelopersInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_developers_info);
+
+        Button button = findViewById(R.id.fromDevToOption);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
