c64f00b96848b170b6491c24f27fd48f1b9b8fcf
H. Jaeh <huhj98@naver.com>
2022-05-20 20:54:14 +0900

    calendar implementation(not working yet)
    added calendar
    (not working)

--- app/src/main/java/com/gachon/morningroutin_layout/GraphActivity.java
+++ app/src/main/java/com/gachon/morningroutin_layout/GraphActivity.java
@@ -1,20 +1,99 @@
 package com.gachon.morningroutin_layout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class GraphActivity extends AppCompatActivity {
+    // create material calendar
+    MaterialCalendarView calendarView;
+    // get instances for firebase database
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    // date arraylist
+    ArrayList<CalendarDay> failDates;
+    ArrayList<CalendarDay> successDates;
+    // get current year, month, day
+    LocalDate currentDate = LocalDate.now();
+    int year = currentDate.getYear();
+    int month = currentDate.getMonthValue();
+    int day = currentDate.getDayOfMonth();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
 
-        //image button
+        calendarView = findViewById(R.id.calendar);
+
+        calendarView.state()
+                .edit()
+                .setCalendarDisplayMode(CalendarMode.WEEKS)
+                .commit();
+
+        DatabaseReference statsRef = firebaseDatabase.getReference();
+        statsRef.child("stats").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int statsArray[] = new int[30];
+                getStats stats = snapshot.getValue(getStats.class);
+                statsArray = stats.getStatsArray();
+
+                for(int i = 0; i < 30; i++){
+                    // if user failed to do routine
+                    if(statsArray[i] == 0){
+                        CalendarDay day = CalendarDay.from(year, month, statsArray[i]);
+                        failDates.add(day);
+                    }
+                    // if user succeed routine
+                    else if(statsArray[i] == 1){
+                        CalendarDay day = CalendarDay.from(year, month, statsArray[i]);
+                        successDates.add(day);
+                    }
+                }
+
+                for(int i = 0; i < failDates.size(); i++){
+                    calendarView.addDecorator(FailDecorator());
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        // image button
         ImageButton main = findViewById(R.id.Plan);
         main.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,5 +149,43 @@ public class GraphActivity extends AppCompatActivity {
                 overridePendingTransition(0, 0);
             }
         });
+    }
+
+    public class FailDecorator implements DayViewDecorator{
+        Drawable fail = getDrawable(R.drawable.fail);
+        private ArrayList<CalendarDay> dates;
+
+        public FailDecorator(ArrayList<CalendarDay> dates){
+            this.dates = dates;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.setBackgroundDrawable(fail);
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            return dates.contains(day);
+        }
+    }
+
+    public class SuccessDecorator implements DayViewDecorator{
+        Drawable success = getDrawable(R.drawable.success);
+        private ArrayList<CalendarDay> dates;
+
+        public SuccessDecorator(ArrayList<CalendarDay> dates){
+            this.dates = dates;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.setBackgroundDrawable(success);
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            return dates.contains(day);
+        }
     }
 }
---
+++ app/src/main/java/com/gachon/morningroutin_layout/getStats.java
@@ -0,0 +1,79 @@
+package com.gachon.morningroutin_layout;
+
+public class getStats {
+    private int day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12,
+            day13, day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25,
+            day26, day27, day28, day29, day30, day31;
+
+    public getStats(){}
+
+    public int getDay01() { return day01; }
+    public int getDay02() { return day02; }
+    public int getDay03() { return day03; }
+    public int getDay04() { return day04; }
+    public int getDay05() { return day05; }
+    public int getDay06() { return day06; }
+    public int getDay07() { return day07; }
+    public int getDay08() { return day08; }
+    public int getDay09() { return day09; }
+    public int getDay10() { return day10; }
+    public int getDay11() { return day11; }
+    public int getDay12() { return day12; }
+    public int getDay13() { return day13; }
+    public int getDay14() { return day14; }
+    public int getDay15() { return day15; }
+    public int getDay16() { return day16; }
+    public int getDay17() { return day17; }
+    public int getDay18() { return day18; }
+    public int getDay19() { return day19; }
+    public int getDay20() { return day20; }
+    public int getDay21() { return day21; }
+    public int getDay22() { return day22; }
+    public int getDay23() { return day23; }
+    public int getDay24() { return day24; }
+    public int getDay25() { return day25; }
+    public int getDay26() { return day26; }
+    public int getDay27() { return day27; }
+    public int getDay28() { return day28; }
+    public int getDay29() { return day29; }
+    public int getDay30() { return day30; }
+    public int getDay31() { return day31; }
+
+    public int[] getStatsArray(){
+        int[] statsArray =  new int[30];
+
+        statsArray[0] = getDay01();
+        statsArray[1] = getDay02();
+        statsArray[2] = getDay03();
+        statsArray[3] = getDay04();
+        statsArray[4] = getDay05();
+        statsArray[5] = getDay06();
+        statsArray[6] = getDay07();
+        statsArray[7] = getDay08();
+        statsArray[8] = getDay09();
+        statsArray[9] = getDay10();
+        statsArray[10] = getDay11();
+        statsArray[11] = getDay12();
+        statsArray[12] = getDay13();
+        statsArray[13] = getDay14();
+        statsArray[14] = getDay15();
+        statsArray[15] = getDay16();
+        statsArray[16] = getDay17();
+        statsArray[17] = getDay18();
+        statsArray[18] = getDay19();
+        statsArray[19] = getDay20();
+        statsArray[20] = getDay21();
+        statsArray[21] = getDay22();
+        statsArray[22] = getDay23();
+        statsArray[23] = getDay24();
+        statsArray[24] = getDay25();
+        statsArray[25] = getDay26();
+        statsArray[26] = getDay27();
+        statsArray[27] = getDay28();
+        statsArray[28] = getDay29();
+        statsArray[29] = getDay30();
+        statsArray[30] = getDay31();
+
+        return statsArray;
+    }
+}
